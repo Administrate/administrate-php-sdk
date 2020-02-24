bc9ed1fb5b15d3a4cf523bd156b6a858c08a1bd4
Jad Khater <jck@administrate.co>
2020-02-24 14:43:28 +0200

    Add Event and Categoy Class sekelton

---
+++ src/Category.php
@@ -0,0 +1,47 @@
+<?php
+namespace Administrate\PhpSdk;
+
+/**
+ * Category
+ *
+ * @package    Administrate\PhpSdk
+ * @author     Jad Khater <jck@administrate.co>
+ */
+class Category {
+
+    /**
+     * Method to Get a single category Info from ID.
+     *
+     * @param  string $id   LMS Category ID
+     *
+     * @return String       JSON Object
+     */
+    public static function load($id) {
+
+    }
+
+    /**
+     * Method to get a set of events by IDs
+     *
+     * @param  array $ids   Array of LMS events Ids
+     *
+     * @return String       JSON Object Array Of LMS Events
+     */
+    public static function loadMultiple($ids) {
+
+    }
+
+    /**
+     * Method to get a set of events by IDs
+     *
+     * @param  integer $page    The page number
+     * @param  integer $perPage The number of items / page
+     *
+     * @return String       JSON Object Array Of LMS Events
+     */
+    public static function loadAll($page = 1, $perPage = 10) {
+
+    }
+
+
+}
