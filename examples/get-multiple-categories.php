aa51b5dbf8606f7e4f31a8515f1ba6e0440fcccf
Jad Khater <jck@administrate.co>
2020-03-05 17:29:45 +0200

    Split categories examples into 2 seperate files

---
+++ examples/get-multiple-categories.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Content-Type: application/json');
+
+require_once 'config.php';
+require_once '../vendor/autoload.php';
+
+use Administrate\PhpSdk\Category;
+
+// $categoriesIds Set this value in config.php
+// $activationParams Set this value in config.php
+
+$activationParams = $activationParams['weblink2'];
+
+if ($_SESSION['portal_token']) {
+    $activationParams['accessToken'] = $_SESSION['portal_token'];
+}
+
+$categoryClass = new Category($activationParams);
+$allCategories = $categoryClass->loadAll(3);
+echo($allCategories);
