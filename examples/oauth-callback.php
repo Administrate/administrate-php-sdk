9676f4c0506b364558eafd598760345a3c68019d
Ali Habib <ahh@getadminstrate.co>
2020-03-10 13:01:49 +0200

    add space around before and after concatenation dot

--- examples/oauth-callback.php
+++ examples/oauth-callback.php
@@ -23,7 +23,7 @@ if ($response) {
     print_r($_SESSION);
     echo "</pre>";
 
-    $refreshLink =  BASE_URL."/examples/oauth-refreshToken.php";
+    $refreshLink =  BASE_URL . "/examples/oauth-refreshToken.php";
     $refreshLink .= "?token=$refreshToken";
 
     echo "<a href='$refreshLink' target='_blank'>Refresh token<a/>";
