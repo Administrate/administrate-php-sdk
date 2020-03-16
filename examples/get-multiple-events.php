ad017735ef1b0e7dd65498fac27cbd6109d8fc49
Ali Habib <ahh@getadminstrate.co>
2020-03-16 14:14:05 +0200

    create events listing class and add examples for each method

---
+++ examples/get-multiple-events.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Content-Type: application/json');
+
+require_once 'config.php';
+require_once '../vendor/autoload.php';
+
+use Administrate\PhpSdk\Event;
+
+// $categoriesIds Set this value in config.php
+// $activationParams Set this value in config.php
+
+$activationParams = $activationParams['weblink2'];
+
+if ($_SESSION['portal_token']) {
+    $activationParams['accessToken'] = $_SESSION['portal_token'];
+}
+
+$eventObj = new Event($activationParams);
+$events = $eventObj->loadAll();
+print_r($events);
