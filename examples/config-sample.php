9a2beec60714b09af2858321d6d44dcdcf3490ac
Ali Habib <ahh@getadminstrate.co>
2020-03-31 18:32:48 +0300

    PLAT-5805 update config file remove multi config arrays

--- examples/config-sample.php
+++ examples/config-sample.php
@@ -2,7 +2,7 @@
 
 define('BASE_URL', '');
 
-// USED In Examples (can be removed if not used)
+// Used In Examples (can be removed if not used)
 $categoriesIds = array();
 $learnerId = '';
 $courseId = '';
