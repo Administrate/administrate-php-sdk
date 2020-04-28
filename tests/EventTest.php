f7f3e07da6980646ca49878f17038202f84f416a
Ali Habib <ahh@getadminstrate.co>
2020-04-28 19:40:11 +0300

    PHP Unit tests create first draft of ubit tests

---
+++ tests/EventTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class EventTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test12312() :void
+    {
+         $this->assertTrue(true);
+    }
+}
