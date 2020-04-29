1db136f4989653a023ee01ce30cca8cf64bfa14f
Ali Habib <ahh@getadminstrate.co>
2020-04-29 15:58:09 +0300

    PLAT-5814 Event Unit Tests create unit tests for events class

--- examples/events/get-single.php
+++ examples/events/get-single.php
@@ -11,7 +11,7 @@ use Administrate\PhpSdk\Event;
 // $weblinkActivationParams Set this value in config.php
 // $return type defined in client Class 'array' -> PHP array, 'obj' -> PHP Object and 'json' -> JSON
 $fields = [];
-$returnType = 'json'; //array, obj, json
+$returnType = 'array'; //array, obj, json
 
 $eventObj = new Event($weblinkActivationParams);
 $event = $eventObj->loadById($eventId, $fields, $returnType);
--- tests/EventTest.php
+++ tests/EventTest.php
@@ -1,16 +1,120 @@
 <?php declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use Administrate\PhpSdk\Event;
 
 final class EventTest extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress(): void
+    public function testLoadSingleEvent(): void
     {
-        $this->assertTrue(true);
+        $weblinkActivationParams = array(
+            'oauthServer' => 'https://portal-auth.administratehq.com',
+            'apiUri' => 'https://weblink-api.administratehq.com/graphql/',
+            'portal' => 'mdbcwl-kfmc.administrateweblink.com',
+            'portalToken' => ''.$_GET['portalToken'].''
+        );
+        $fields = [];
+        $returnType = 'array'; //array, obj, json
+        $event = new Event();
+        $eventObj = new Event($weblinkActivationParams);
+        $eventArray = $eventObj->loadById($_GET['eventId'], $fields, 'array');
+        $eventJson = $eventObj->loadById($_GET['eventId'], $fields, 'json');
+        $eventObj = $eventObj->loadById($_GET['eventId'], $fields, 'obj');
+
+        //check response is a php array
+        $this->assertisArray($eventArray);
+        //check response is in json format
+        $this->assertTrue($this->is_json($eventJson));
+        //check response is a pHP object
+        $this->assertisObject($eventObj);
     }
 
-    public function test12312() :void
+    public function testLoadMultipleEvent(): void
     {
-         $this->assertTrue(true);
+        $weblinkActivationParams = array(
+            'oauthServer' => 'https://portal-auth.administratehq.com',
+            'apiUri' => 'https://weblink-api.administratehq.com/graphql/',
+            'portal' => 'mdbcwl-kfmc.administrateweblink.com',
+            'portalToken' => ''.$_GET['portalToken'].''
+        );
+        $fields = [];
+        $event = new Event();
+        
+        $fields = [];
+        $paging = ['page' => 1, 'perPage' => 25];
+        $sorting = ['field' => 'title', 'direction' => 'asc'];
+        $filters = [];
+
+        $eventObj = new Event($weblinkActivationParams);
+        $resultArray = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'array');
+        $resultJson = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'json');
+        $resultObject = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'obj');
+
+        //check response is a php array
+        $this->assertisArray($resultArray);
+        //check response is in json format
+        $this->assertTrue($this->is_json($resultJson));
+        //check response is a pHP object
+        $this->assertisObject($resultObject);
+    }
+
+    public function testLoadEventsbyCourseCode(): void
+    {
+        $weblinkActivationParams = array(
+            'oauthServer' => 'https://portal-auth.administratehq.com',
+            'apiUri' => 'https://weblink-api.administratehq.com/graphql/',
+            'portal' => 'mdbcwl-kfmc.administrateweblink.com',
+            'portalToken' => ''.$_GET['portalToken'].''
+        );
+        $fields = [];
+        $event = new Event();
+        
+        $fields = [];
+        $paging = ['page' => 1, 'perPage' => 25];
+        $sorting = ['field' => 'title', 'direction' => 'asc'];
+        $filters = ['courseCode' => $courseCode];
+
+        $eventObj = new Event($weblinkActivationParams);
+        $events = $eventObj->loadByCourseCode($filters, $paging, $sorting, $fields, $returnType);
+
+        $resultArray = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'array');
+        $resultJson = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'json');
+        $resultObject = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'obj');
+
+        //check response is a php array
+        $this->assertisArray($resultArray);
+        //check response is in json format
+        $this->assertTrue($this->is_json($resultJson));
+        //check response is a pHP object
+        $this->assertisObject($resultObject);
+    }
+
+    public function testPagination(): void
+    {
+            $weblinkActivationParams = array(
+            'oauthServer' => 'https://portal-auth.administratehq.com',
+            'apiUri' => 'https://weblink-api.administratehq.com/graphql/',
+            'portal' => 'mdbcwl-kfmc.administrateweblink.com',
+            'portalToken' => ''.$_GET['portalToken'].''
+            );
+                $fields = [];
+                $event = new Event();
+        
+                $fields = [];
+                $paging = ['page' => 1, 'perPage' => 25];
+                $sorting = ['field' => 'title', 'direction' => 'asc'];
+                $filters = [];
+
+                $eventObj = new Event($weblinkActivationParams);
+                $events = $eventObj->loadAll($filters, $paging, $sorting, $fields, $returnType);
+
+                $resultArray = $eventObj->loadAll($filters, $paging, $sorting, $fields, 'array');
+                //check if pagination returns the requested number of results
+                $this->assertEquals(27, count($resultArray['events']['edges']), 'Error in pagination results');
+    }
+
+    public function is_json($str)
+    {
+        return json_decode($str) != null;
     }
 }
