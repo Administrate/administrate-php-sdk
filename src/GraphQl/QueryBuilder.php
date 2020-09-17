911c304404603f0402eb4e2d550eda4247360333
Ali Habib <ahh@getadminstrate.co>
2020-09-17 14:57:59 +0300

    PLAT-7828 query builder accepts sub selections
    after this change we can pass nested arrays as params so when there is a sub selection we can return the inner nodes

--- src/GraphQl/QueryBuilder.php
+++ src/GraphQl/QueryBuilder.php
@@ -11,9 +11,21 @@ class QueryBuilder extends GqlQueryBuilder
         $node = (new QueryBuilder('node'));
         foreach ($fields as $fieldKey => $fieldVal) {
             if (is_array($fieldVal)) {
-                $subNode = (new QueryBuilder('' . $fieldKey . ''));
-                foreach ($fieldVal as $subFieldKey) {
-                    $subNode->selectField($subFieldKey);
+                if (isset($fieldVal['type']) && $fieldVal['type'] == 'edges') {
+                    $fields = $fieldVal['fields'];
+                    $subNode = (new QueryBuilder('' . $fieldKey . ''));
+                    $edgesNode = (new QueryBuilder('edges'));
+                    $innerNode = (new QueryBuilder('node'));
+                    foreach ($fields as $subFieldKey) {
+                        $innerNode->selectField($subFieldKey);
+                    }
+                    $edgesNode->selectField($innerNode);
+                    $subNode->selectField($edgesNode);
+                } else {
+                    $subNode = (new QueryBuilder('' . $fieldKey . ''));
+                    foreach ($fieldVal as $subFieldKey) {
+                        $subNode->selectField($subFieldKey);
+                    }
                 }
                 $node->selectField($subNode);
             } else {
